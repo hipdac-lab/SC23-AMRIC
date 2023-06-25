6ee77491cc71a68a62a54b32e5962fe57a4d22a0
Chameleon Cloud User <cc@daoce-sc23.novalocal>
2023-06-25 20:43:47 +0000

    mpi-include

--- openmpi.sh
+++ openmpi.sh
@@ -15,4 +15,6 @@
     export PATH=$OMPI_DIR/bin:$PATH
     export LD_LIBRARY_PATH=$OMPI_DIR/lib:$LD_LIBRARY_PATH
     export MANPATH=$OMPI_DIR/share/man:$MANPATH
+    export C_INCLUDE_PATH=/opt/ompi/include:$C_INCLUDE_PATH
+    export CPLUS_INCLUDE_PATH=/opt/ompi/include:$CPLUS_INCLUDE_PATH
 
