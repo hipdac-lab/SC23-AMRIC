26f49ade988d881b9064eeb78516f189764cb227
FabioGrosso <37393451+FabioGrosso@users.noreply.github.com>
2023-06-24 19:28:51 -0700

    Update compressor.sh

--- compressor.sh
+++ compressor.sh
@@ -1,6 +1,7 @@
 cd SZ_SLE
 mkdir install
-mkdir build && cd build
+mkdir build
+cd build
 cmake -DCMAKE_INSTALL_PREFIX:PATH=$AMRIC_HOME/SZ_SLE/install -DBUILD_H5Z_FILTER=ON ..
 export SZ_SLE_HOME=$AMRIC_HOME/SZ_SLE/install
 make -j 8
@@ -9,7 +10,8 @@ make install
 cd $AMRIC_HOME
 cd SZ3
 mkdir install
-mkdir build && cd build
+mkdir build
+cd build
 cmake -DCMAKE_INSTALL_PREFIX:PATH=$AMRIC_HOME/SZ3/install -DBUILD_H5Z_FILTER=ON ..
 export SZ3_HOME=$AMRIC_HOME/SZ3/install
 make -j 8
@@ -18,7 +20,8 @@ make install
 cd $AMRIC_HOME
 cd orisz3
 mkdir install
-mkdir build && cd build
+mkdir build
+cd build
 cmake -DCMAKE_INSTALL_PREFIX:PATH=$AMRIC_HOME/orisz3/install -DBUILD_H5Z_FILTER=ON ..
 export ORI_SZ3_HOME=$AMRIC_HOME/orisz3/install 
 make -j 8
