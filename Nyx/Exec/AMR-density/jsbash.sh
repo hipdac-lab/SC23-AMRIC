6e51929a9d0cbb146a44ad27594cf2042b823429
Daoce Wang <daoce@login3.summit.olcf.ornl.gov>
2023-06-25 13:57:41 -0400

    bash

--- Nyx/Exec/AMR-density/jsbash.sh
+++ Nyx/Exec/AMR-density/jsbash.sh
@@ -5,23 +5,23 @@ do
     echo "0.001" > eb.txt
     echo "sz2"
     source ./sz2.sh
-    mpirun -np 8 ./sz2 inputs > otfile/sz2-$i.txt
+    jsrun -n 8 ./sz2 inputs > otfile/sz2-$i.txt
     mv run/plt00211_multi.h5 run/sz2-001.h5
 
     echo "0.001" > eb.txt
     echo "sz3"
     source ./sz3.sh
-    mpirun -np 8 ./sz3 inputs > otfile/sz3-$i.txt
+    jsrun -n 8 ./sz3 inputs > otfile/sz3-$i.txt
     mv run/plt00211_multi.h5  run/sz3-001.h5
 
     echo "0.005" > eb.txt
     source ./base.sh
     echo "nocomp"
-    mpirun -np 8 ./nocomp inputs > otfile/nocomp-$i.txt
+    jsrun -n 8 ./nocomp inputs > otfile/nocomp-$i.txt
     mv run/plt00211_multi.h5  run/nocomp.h5
 
     echo "comp"
-    mpirun -np 8 ./comp inputs > otfile/comp-$i.txt
+    jsrun -n 8 ./comp inputs > otfile/comp-$i.txt
     mv run/plt00211_multi.h5  run/comp-005.h5
 done
 rm -rf run/*old*
