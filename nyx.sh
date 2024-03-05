c428b2de3dc1bf7b7a645a1489c745ad43b6c385
FabioGrosso <37393451+FabioGrosso@users.noreply.github.com>
2024-03-05 13:48:45 -0500

    Update nyx.sh

--- nyx.sh
+++ nyx.sh
@@ -4,10 +4,15 @@ cd Nyx/subprojects
 . build-sun.sh
 
 cd $AMRIC_HOME/Nyx/Exec/AMR-density
+# make clean
+# cp $AMRIC_HOME/amrex/Src/Extern/HDF5/AMReX_PlotFileUtilHDF5.nast $AMRIC_HOME/amrex/Src/Extern/HDF5/AMReX_PlotFileUtilHDF5.cpp
+# make -j 16 USE_HDF5_SZ3=TRUE SZ3_HOME=$SZ_SLE_HOME
+# mv Nyx3d* sz2
+
 make clean
-cp $AMRIC_HOME/amrex/Src/Extern/HDF5/AMReX_PlotFileUtilHDF5.nast $AMRIC_HOME/amrex/Src/Extern/HDF5/AMReX_PlotFileUtilHDF5.cpp
-make -j 16 USE_HDF5_SZ3=TRUE SZ3_HOME=$SZ_SLE_HOME
-mv Nyx3d* sz2
+cp $AMRIC_HOME/amrex/Src/Extern/HDF5/AMReX_PlotFileUtilHDF5.pad $AMRIC_HOME/amrex/Src/Extern/HDF5/AMReX_PlotFileUtilHDF5.cpp
+make -j 16 USE_HDF5_SZ3=TRUE SZ3_HOME=$SZ3_HOME-adp
+mv Nyx3d* pad
 
 make clean
 cp $AMRIC_HOME/amrex/Src/Extern/HDF5/AMReX_PlotFileUtilHDF5.stack $AMRIC_HOME/amrex/Src/Extern/HDF5/AMReX_PlotFileUtilHDF5.cpp
@@ -19,10 +24,10 @@ cp $AMRIC_HOME/amrex/Src/Extern/HDF5/AMReX_PlotFileUtilHDF5.ori $AMRIC_HOME/amre
 make -j 16 USE_HDF5_SZ3=FALSE 
 mv Nyx3d* nocomp
 
-make clean
-cp $AMRIC_HOME/amrex/Src/Extern/HDF5/AMReX_PlotFileUtilHDF5.ori $AMRIC_HOME/amrex/Src/Extern/HDF5/AMReX_PlotFileUtilHDF5.cpp
-make -j 16 USE_HDF5_SZ3=TRUE SZ3_HOME=$ORI_SZ3_HOME
-mv Nyx3d* comp
+# make clean
+# cp $AMRIC_HOME/amrex/Src/Extern/HDF5/AMReX_PlotFileUtilHDF5.ori $AMRIC_HOME/amrex/Src/Extern/HDF5/AMReX_PlotFileUtilHDF5.cpp
+# make -j 16 USE_HDF5_SZ3=TRUE SZ3_HOME=$ORI_SZ3_HOME
+# mv Nyx3d* comp
 
 cd $AMRIC_HOME
 
