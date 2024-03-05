ffd8b5b85b6c7b35df0fa9b9367b5b1353e4fb7e
FabioGrosso <37393451+FabioGrosso@users.noreply.github.com>
2024-03-05 13:49:44 -0500

    Update nyx.sh

--- nyx.sh
+++ nyx.sh
@@ -9,10 +9,10 @@ cp $AMRIC_HOME/amrex/Src/Extern/HDF5/AMReX_PlotFileUtilHDF5.nast $AMRIC_HOME/amr
 make -j 16 USE_HDF5_SZ3=TRUE SZ3_HOME=$SZ_SLE_HOME
 mv Nyx3d* sz2
 
-make clean
-cp $AMRIC_HOME/amrex/Src/Extern/HDF5/AMReX_PlotFileUtilHDF5.pad $AMRIC_HOME/amrex/Src/Extern/HDF5/AMReX_PlotFileUtilHDF5.cpp
-make -j 16 USE_HDF5_SZ3=TRUE SZ3_HOME=$SZ3_HOME-adp
-mv Nyx3d* pad
+# make clean
+# cp $AMRIC_HOME/amrex/Src/Extern/HDF5/AMReX_PlotFileUtilHDF5.pad $AMRIC_HOME/amrex/Src/Extern/HDF5/AMReX_PlotFileUtilHDF5.cpp
+# make -j 16 USE_HDF5_SZ3=TRUE SZ3_HOME=$SZ3_HOME-adp
+# mv Nyx3d* pad
 
 make clean
 cp $AMRIC_HOME/amrex/Src/Extern/HDF5/AMReX_PlotFileUtilHDF5.stack $AMRIC_HOME/amrex/Src/Extern/HDF5/AMReX_PlotFileUtilHDF5.cpp
